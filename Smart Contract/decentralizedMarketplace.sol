625fe6d264998c564b2a53e3154faf6201dd3895
Farhana Javed <87488893+farhanajaved@users.noreply.github.com>
2023-05-30 17:58:01 +0200

    Create decentralizedMarketplace.sol
    The provided smart contract, "TokenizedIdentity", facilitates the registration and tokenized identity management of consumers and providers in a decentralized ecosystem. It leverages the Ethereum blockchain to enable secure and transparent interactions. The contract offers the following features:
    
    1. Registration: Consumers and providers can register themselves by calling the respective registration functions. Once registered, their addresses are stored in the contract.
    
    2. Tokenized Identity: When both a consumer and a provider register, their tokenized identity is established. This indicates that they have completed the registration process and are eligible to interact within the system. The contract keeps track of the tokenized identities through the "hasTokenizedIdentity" mapping.
    
    3. IPFS Hash Access: Registered consumers and providers can access their IPFS hash by calling the "getHash()" function. This allows them to retrieve a unique hash associated with their account. The IPFS hash could be used to store additional information, such as documents or metadata relevant to their identity or transactions.
    
    4. IPFS Hash Update: Consumers have the ability to update their IPFS hash by invoking the "setHash()" function. This feature enables consumers to modify and maintain the information linked to their account over time.
    
    By utilizing this smart contract, developers can create decentralized applications or platforms that require identity management and secure data retrieval using IPFS. The immutability and transparency of the Ethereum blockchain ensure the integrity and authenticity of registered identities and associated IPFS hashes.

---
+++ Smart Contract/decentralizedMarketplace.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.0;
+
+contract TokenizedIdentity {
+    mapping(address => bool) public isConsumerRegistered;
+    mapping(address => bool) public isProviderRegistered;
+    mapping(address => bool) public hasTokenizedIdentity;
+    mapping(address => string) public ipfsHashes;
+
+    event ConsumerRegistered(address consumer);
+    event ProviderRegistered(address provider);
+
+    modifier onlyRegisteredConsumer() {
+        require(isConsumerRegistered[msg.sender], "Only registered consumers can access this functionality");
+        _;
+    }
+
+    modifier onlyRegisteredProvider() {
+        require(isProviderRegistered[msg.sender], "Only registered providers can access this functionality");
+        _;
+    }
+
+    function registerConsumer() external {
+        isConsumerRegistered[msg.sender] = true;
+        emit ConsumerRegistered(msg.sender);
+        if (isProviderRegistered[msg.sender])
+            hasTokenizedIdentity[msg.sender] = true;
+    }
+
+    function registerProvider() external {
+        isProviderRegistered[msg.sender] = true;
+        emit ProviderRegistered(msg.sender);
+        if (isConsumerRegistered[msg.sender])
+            hasTokenizedIdentity[msg.sender] = true;
+    }
+
+    function getHash() external view onlyRegisteredConsumer returns (string memory) {
+        return ipfsHashes[msg.sender];
+    }
+
+    function setHash(string memory hash) external onlyRegisteredConsumer {
+        ipfsHashes[msg.sender] = hash;
+    }
+}
