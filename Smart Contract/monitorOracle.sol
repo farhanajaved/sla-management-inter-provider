3e3eafd988c9a09de7bac1c446341d088aabf21d
Farhana Javed <87488893+farhanajaved@users.noreply.github.com>
2023-05-30 18:52:56 +0200

    Create monitorOracle.sol

---
+++ Smart Contract/monitorOracle.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.8.0;
+
+import "./DecentralizedMarketplace.sol";
+import "./ReverseAuction.sol";
+import "./BreachSLA.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract MonitorOracle is DecentralizedMarketplace, ReverseAuction, BreachSLA,  ChainlinkClient {
+    string public bytesData;
+    uint256 public uintData;
+    string public endpoint;
+
+    event DataFetched(string data, uint256 timestamp);
+    event EndpointSet(string endpoint);
+
+    modifier onlyAuthorized() {
+        require(
+            consumers[msg.sender].isRegistered || providers[msg.sender].isRegistered,
+            "Only authorized users can access this functionality"
+        );
+        _;
+    }
+
+    constructor() {
+        setPublicChainlinkToken();
+    }
+
+    function fetchData() external onlyAuthorized {
+        // Chainlink automation for fetching data from API goes here
+        // More details about Chainlink automation can be found on their website: [https://chain.link/automation]
+
+        // Mock implementation for demonstration purposes
+        bytesData = "Sample bytes data";
+        uintData = 123;
+        emit DataFetched(bytesData, block.timestamp);
+    }
+
+    function showData() external view onlyAuthorized returns (string memory, uint256) {
+        return (bytesData, uintData);
+    }
+
+    function setEndpoint(string memory _endpoint) external onlyAuthorized {
+        endpoint = _endpoint;
+        emit EndpointSet(endpoint);
+    }
+}
